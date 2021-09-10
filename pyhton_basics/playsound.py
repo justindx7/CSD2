43df1c85321741c92ff81ffd44ca3d7f16863f41
justin bonnet <justin.albonnet@gmail.com>
2021-09-10 15:02:42 +0200

    added user input and while loop for multiple sample plays

--- pyhton_basics/playsound.py
+++ pyhton_basics/playsound.py
@@ -1,5 +1,12 @@
 import simpleaudio as sa
 
-wave_obj = sa.WaveObject.from_wave_file("../Samples/kick.wav")
-play_object = wave_obj.play()
-play_object.wait_done()
+print("how many times should the sample be played?")
+x = input("")
+
+i = 1
+
+while i<=int(x):
+    wave_obj = sa.WaveObject.from_wave_file("../Samples/kick.wav")
+    play_object = wave_obj.play()
+    play_object.wait_done()
+    i=i+1
