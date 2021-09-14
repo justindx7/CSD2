4e80b01f64d12c873cdfb1183e3b068d1998b0ab
justin bonnet <justin.albonnet@gmail.com>
2021-09-14 20:00:38 +0200

    Added code to play samples

--- pyhton_basics/rhythmicplayback.py
+++ pyhton_basics/rhythmicplayback.py
@@ -1,3 +1,4 @@
+import time
 import simpleaudio as sa
 
 noteTimes = []
@@ -7,17 +8,24 @@ kick = sa.WaveObject.from_wave_file("../Samples/kick.wav")
 print("how many times should the sample be played?")
 numPlaybackTimes = input("")
 
-
 print("Enter " + numPlaybackTimes + " note times")
+
+# read playback times 
 i = 1
 while i<=int(numPlaybackTimes):
     y = input("")
     noteTimes.append(y)
-    i=i+1
+    i+=1
 
 print(noteTimes)
+
 print("BPM:")
 bpm = input("")
 
+# calculate sample length
+ms = 60/int(bpm)
 
-
+# play samples
+for t in noteTimes:
+    kick.play()
+    time.sleep(float(t) * float(ms))
