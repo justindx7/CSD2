6855b1da6a07f3733418cf6aeb9b86d3193f988c
justin bonnet <justin.albonnet@gmail.com>
2021-09-24 13:40:45 +0200

    Added more descriptive comments

--- CSD2a/single_sample_sequencer/src/single_sample_sequencer.py
+++ CSD2a/single_sample_sequencer/src/single_sample_sequencer.py
@@ -6,12 +6,13 @@ import simpleaudio as sa
 kick = sa.WaveObject.from_wave_file("../../../Samples/kick.wav")
 
 bpm = 120
-
-
 print("Default bpm: ",bpm)
+
+
 noteDurations = [0.25, 0.5, 0.25, 0.5, 0.5, 1, 1]
 
 
+# [Y/n] question to change bpm
 if click.confirm('Do you want to edit the bpm?', default=True):
     while True:
         bpm = input("")
@@ -26,6 +27,7 @@ quarterNoteDuration = 60 / int(bpm)
 sixteenthNoteDuration = quarterNoteDuration / 4.0
 
 
+# function to turn noteDurations into 16ths timestamps (make a 16th grid)
 def durationsToTimestamps16th(note_Durations):
     timestamps_16th = []
     sum = 0 
@@ -34,31 +36,40 @@ def durationsToTimestamps16th(note_Durations):
         sum = sum + i * 4
     return timestamps_16th
 
+# create list with 16ths timestamps calculated in the function 
 timestamps16th = durationsToTimestamps16th(noteDurations)
 print("16TH:", timestamps16th)
 
+
+# function to convert the 16ths timestamps into ms
 def timestampsToDelay(timestamps_16th, _BPM):
     time_Stamps = []
     for i in timestamps_16th:
        time_Stamps.append(i * sixteenthNoteDuration)
     return time_Stamps
-    
+
+# create list with ms timestamps   
 timeStamps = timestampsToDelay(timestamps16th, bpm)
 print("16TH Time stamp:", timeStamps)
 
   
 
 zero_time = time.time()
+# the i is used to determen the time stamp that should be used
+
 i = 0
+# start a counter and look if the timestamps list is equal to the current time 
 while True:
     now = time.time() - zero_time
     if(now >= float(timeStamps[i])):
         kick.play()
         print("kick", i)
         i = i + 1
+        # if the i is at the end of the given list stop the loop
         if i == len(timeStamps):
             break
     time.sleep(0.001)
-     
+
+# wait until the last sample is done playing     
 time.sleep(timeStamps[-1])
 
