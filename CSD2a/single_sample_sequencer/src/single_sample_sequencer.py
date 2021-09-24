65efc851ca1cce5c393dd2398cf7b023495ed52f
justin bonnet <justin.albonnet@gmail.com>
2021-09-24 13:33:25 +0200

    added sample playback and fixed functions in and output

--- CSD2a/single_sample_sequencer/src/single_sample_sequencer.py
+++ CSD2a/single_sample_sequencer/src/single_sample_sequencer.py
@@ -1,34 +1,16 @@
-from time import time
+import time
 import click
+import simpleaudio as sa
+
+# load audio sample
+kick = sa.WaveObject.from_wave_file("../../../Samples/kick.wav")
+
 bpm = 120
 
-quarterNoteDuration = 60 / bpm
-sixteenthNoteDuration = quarterNoteDuration / 4.0
 
-timeStamps = []
 print("Default bpm: ",bpm)
 noteDurations = [0.25, 0.5, 0.25, 0.5, 0.5, 1, 1]
 
-timestamps16th = []
-
-
-def durationsToTimestamps16th():
-    sum = 0
-    for i in noteDurations:
-        timestamps16th.append(sum)
-        sum = sum + i * 4
-
-
-durationsToTimestamps16th()
-print("16TH:", timestamps16th)
-
-def timestampsToDelay():
-   for i in timestamps16th:
-       timeStamps.append(i * sixteenthNoteDuration)
-    
-  
-timestampsToDelay()
-print("16TH Time stamp:", timeStamps)
 
 if click.confirm('Do you want to edit the bpm?', default=True):
     while True:
@@ -38,10 +20,45 @@ if click.confirm('Do you want to edit the bpm?', default=True):
             print("BPM: ",bpm)
             break
         except ValueError:
-            print("Value was not correct syntax try again")        
+            print("Value was not correct syntax try again")  
 
-i = 1
-while i < len(timeStamps):
-    time = timeStamps[i] - timeStamps[i-1]
-    print("time: ", time)
-    i += 1     
+quarterNoteDuration = 60 / int(bpm)
+sixteenthNoteDuration = quarterNoteDuration / 4.0
+
+
+def durationsToTimestamps16th(note_Durations):
+    timestamps_16th = []
+    sum = 0 
+    for i in note_Durations:
+        timestamps_16th.append(sum)
+        sum = sum + i * 4
+    return timestamps_16th
+
+timestamps16th = durationsToTimestamps16th(noteDurations)
+print("16TH:", timestamps16th)
+
+def timestampsToDelay(timestamps_16th, _BPM):
+    time_Stamps = []
+    for i in timestamps_16th:
+       time_Stamps.append(i * sixteenthNoteDuration)
+    return time_Stamps
+    
+timeStamps = timestampsToDelay(timestamps16th, bpm)
+print("16TH Time stamp:", timeStamps)
+
+  
+
+zero_time = time.time()
+i = 0
+while True:
+    now = time.time() - zero_time
+    if(now >= float(timeStamps[i])):
+        kick.play()
+        print("kick", i)
+        i = i + 1
+        if i == len(timeStamps):
+            break
+    time.sleep(0.001)
+     
+time.sleep(timeStamps[-1])
+
