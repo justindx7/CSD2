256983d410cdaeb7f9db694f26eea83e28689d47
justin bonnet <justin.albonnet@gmail.com>
2021-09-28 13:21:20 +0200

    Fixed bpm input in timestampsToDelay

--- CSD2a/single_sample_sequencer/src/single_sample_sequencer.py
+++ CSD2a/single_sample_sequencer/src/single_sample_sequencer.py
@@ -2,15 +2,32 @@ import time
 import click
 import simpleaudio as sa
 
+# function to turn noteDurations into 16ths timestamps (make a 16th grid)
+def durationsToTimestamps16th(note_Durations):
+    timestamps_16th = []
+    sum = 0 
+    for i in note_Durations:
+        timestamps_16th.append(sum)
+        sum = sum + i * 4
+    return timestamps_16th
+
+# function to convert the 16ths timestamps into ms
+def timestampsToDelay(timestamps_16th, _BPM):
+    quarterNoteDuration = 60 / int(_BPM)
+    sixteenthNoteDuration = quarterNoteDuration / 4.0
+    time_Stamps = []
+    for i in timestamps_16th:
+       time_Stamps.append(i * sixteenthNoteDuration)
+    return time_Stamps
+
+
 # load audio sample
 kick = sa.WaveObject.from_wave_file("../../../Samples/kick.wav")
 
 bpm = 120
 print("Default bpm: ",bpm)
 
-
 noteDurations = [0.25, 0.5, 0.25, 0.5, 0.5, 1, 1]
-
 
 # [Y/n] question to change bpm
 if click.confirm('Do you want to edit the bpm?', default=True):
@@ -23,42 +40,22 @@ if click.confirm('Do you want to edit the bpm?', default=True):
         except ValueError:
             print("Value was not correct syntax try again")  
 
-quarterNoteDuration = 60 / int(bpm)
-sixteenthNoteDuration = quarterNoteDuration / 4.0
 
-
-# function to turn noteDurations into 16ths timestamps (make a 16th grid)
-def durationsToTimestamps16th(note_Durations):
-    timestamps_16th = []
-    sum = 0 
-    for i in note_Durations:
-        timestamps_16th.append(sum)
-        sum = sum + i * 4
-    return timestamps_16th
 
 # create list with 16ths timestamps calculated in the function 
 timestamps16th = durationsToTimestamps16th(noteDurations)
 print("16TH:", timestamps16th)
 
-
-# function to convert the 16ths timestamps into ms
-def timestampsToDelay(timestamps_16th, _BPM):
-    time_Stamps = []
-    for i in timestamps_16th:
-       time_Stamps.append(i * sixteenthNoteDuration)
-    return time_Stamps
-
-# create list with ms timestamps   
+# create list with ms timestamps calculated in the function
 timeStamps = timestampsToDelay(timestamps16th, bpm)
 print("16TH Time stamp:", timeStamps)
 
-  
-
 zero_time = time.time()
-# the i is used to determen the time stamp that should be used
 
+# the i is used to determine the time stamp that should be used
 i = 0
-# start a counter and look if the timestamps list is equal to the current time 
+
+# start a time counter and look if the timestamps list is equal to the current time 
 while True:
     now = time.time() - zero_time
     if(now >= float(timeStamps[i])):
@@ -68,8 +65,8 @@ while True:
         # if the i is at the end of the given list stop the loop
         if i == len(timeStamps):
             break
+    # update time
     time.sleep(0.001)
-
 # wait until the last sample is done playing     
 time.sleep(timeStamps[-1])
 
