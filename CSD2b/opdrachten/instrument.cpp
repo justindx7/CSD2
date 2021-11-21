7ef5ffd721c0837d7efd1bcd00ff0092651a90c8
justin bonnet <justin.albonnet@gmail.com>
2021-11-21 01:35:27 +0100

    Added comments

--- CSD2b/opdrachten/instrument.cpp
+++ CSD2b/opdrachten/instrument.cpp
@@ -3,13 +3,15 @@
 
 class Instrument {
 public:
+    // explicit so you can only write a instrument(string) and not just a string.
     explicit Instrument(std::string _sound);
     ~Instrument();
     void play();
+//this is private using an instance to change the value
 private:
     std::string sound;
 };
-
+// : sound(_sound) == sound = _sound 
 Instrument::Instrument(std::string _sound): sound(_sound)
 {
     std::cout <<"Concustructor called" << std::endl;
