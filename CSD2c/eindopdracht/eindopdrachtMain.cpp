e18e920a503985d5c6c36de38eb79d616f3da403
Justin Bonnet <justin.albonnet@gmail.com>
2022-03-22 19:50:08 +0100

    added change of feedback on peak detection

--- CSD2c/eindopdracht/eindopdrachtMain.cpp
+++ CSD2c/eindopdracht/eindopdrachtMain.cpp
@@ -6,9 +6,8 @@
 #include "effect.h"
 #include "chorus.h"
 #include "analyzer.h"
-
+#include <cmath>
 #define WRITE_NUM_SAMPLES 44100
-
 // boolean is used to keep program running / turn it off
 bool running = true;
 
@@ -22,67 +21,91 @@ float amplitude = 0.5;
 // create a JackModule instance
 JackModule jack;
 
-std::vector <Effect *> effects{};
+std::vector<Effect *> effects{};
 
 Analyzer analyzer(samplerate);
 
-static void connectToJack()
+
+bool peaked= false;
+int effectCounter = 0;
+
+void changeEffects(int counter, float buf)
 {
-    float *inBuf = new float[chunksize];
-    float *outBuf = new float[chunksize];
-    int counter = 0;
-    do
+   double feedbackAmount;
+   //scale counter to feedback amount
+   feedbackAmount = (0.4 - 0.9)*(effectCounter)/(samplerate * 6) + 0.9;
+
+   //round feedbackAmount to 3 decimals
+   feedbackAmount = round(feedbackAmount * 1000.0 ) / 1000.0;
+  
+  if (analyzer.getPeak())
+  {
+    peaked = true;
+  } 
+
+  if(peaked)
+  {
+    effectCounter++;
+    // 0.4  // /2.25 //117600
+    effects[0]->setParameter("feedback", feedbackAmount);
+    effects[1]->setParameter("feedback", feedbackAmount);
+
+    if(feedbackAmount < 0.41)
     {
-        jack.readSamples(inBuf, chunksize);
-        for (unsigned int i = 0; i < chunksize; i++)
-        {
-            // LEFT CHANNEL
-            outBuf[2 * i] = effects[0]->process(inBuf[i] * amplitude); // effect code here
+    peaked = false;
+    effects[0]->setParameter("feedback", 0.4);
+    effects[1]->setParameter("feedback", 0.4);
+    effectCounter = 0;
+    }
+  }
 
-            // RIGHT CHANNEL
-            outBuf[2 * i + 1] =effects[1]->process(inBuf[i] * amplitude); // effect code here
+}
 
-            analyzer.analyseSignal(inBuf[i]);
-            counter++;
-            if(counter >= samplerate * 4){counter = 0;}
-            //TODO use srand to change feedback values
-            if(analyzer.getPeak())
-            {
-              effects[0]->setParameter("freq", (inBuf[i] * 1000)/5);
-              effects[1]->setParameter("freq", (inBuf[i] * 1000)/5);
-              std::cout<< 40 / (inBuf[i] * 10) << std::endl;
-            }
-            if(!analyzer.getPeak())
-            {
-              if(counter == samplerate * 2)
-              {
-              effects[0]->setParameter("freq", 0.4);
-              effects[1]->setParameter("freq", 0.8);
-              }
-            }
-        }
-        jack.writeSamples(outBuf, chunksize * 2);
-    } while (running);
+static void processAudio()
+{
+  float *inBuf = new float[chunksize];
+  float *outBuf = new float[chunksize];
+  int counter = 0;
+  do
+  {
+    jack.readSamples(inBuf, chunksize);
+    for (unsigned int i = 0; i < chunksize; i++)
+    {
+      // LEFT CHANNEL
+      outBuf[2 * i] = effects[0]->process(inBuf[i] * amplitude); // effect code here
+
+      // RIGHT CHANNEL
+      outBuf[2 * i + 1] = effects[1]->process(inBuf[i] * amplitude); // effect code here
+
+
+      // TODO maybe not analyze the signal in the processAudio 
+      analyzer.analyseSignal(inBuf[i]);
+      counter++;
+
+      changeEffects(counter, inBuf[i]);
+      if (counter >= samplerate * 6){counter = 0;}
+    }
+    jack.writeSamples(outBuf, chunksize * 2);
+  } while (running);
 }
 
 int main(int argc, char **argv)
 {
-    // set the amount of interleaved jack channels
-    jack.setNumberOfInputChannels(1);
-    jack.setNumberOfOutputChannels(2);
+  // set the amount of interleaved jack channels
+  jack.setNumberOfInputChannels(1);
+  jack.setNumberOfOutputChannels(2);
 
-    // init the jack, use program name as JACK client name
-    jack.init(argv[0]);
-    jack.autoConnect();
-    samplerate = jack.getSamplerate();
+  // init the jack, use program name as JACK client name
+  jack.init(argv[0]);
+  jack.autoConnect();
+  samplerate = jack.getSamplerate();
 
+  // here we fill the effect vector with effects
+  effects.push_back(new Chorus(0.5, false, samplerate, 0.4, true, 0.4));
+  effects.push_back(new Chorus(0.5, false, samplerate, 0.4, false, 0.8));
 
-    //here we fill the effect vector with effects
-    effects.push_back(new Chorus(0.5, false, samplerate, 0.4, true, 0.4));
-    effects.push_back(new Chorus(0.5, false, samplerate, 0.4, false, 0.8));  
-
-    //new thread 
-    std::thread jackThread(connectToJack);
+  // new thread
+  std::thread jackThread(processAudio);
 
   std::cout << "\n\nPress 'q' when you want to end the program.\n";
   while (running)
@@ -93,7 +116,7 @@ int main(int argc, char **argv)
       running = false;
       break;
     }
-    //otherwise i get segmentation fault when closing the program
+    // otherwise i get segmentation fault when closing the program
     usleep(100000);
   }
   // end the program
