2a60c88d22c4a18c35cb0fa0064f0b8b390770a2
julia kuipers <juliakuipers64@gmail.com>
2022-03-27 09:04:55 +0200

    made final changes to looer

--- CSD2c/eindopdracht/looper.cpp
+++ CSD2c/eindopdracht/looper.cpp
@@ -1,16 +1,18 @@
 #include "sine.h"
+#include "saw.h"
+#include "triangle.h"
 #include "looper.h"
 AudioFile<float> audioFile;
 
+//to use this effect setParameter should be called first, to load a wav file
 Looper::Looper(float drywet, bool bypass, unsigned int samplerate) :
 Effect(drywet,bypass,samplerate),
-currentSample(0)
+currentSample(0),numSamples(0) ,modFreq(1)
 {
-  std::cout << "Looper - Constructor " << std::endl;
-  osc = new Sine(1,samplerate);
-  writeFile = new WriteToFile("output.csv",true);
-  fillBuffer();
+  std::cout << "looper" << std::endl;
 }
+//LFO in
+//select waveform
 
 Looper::~Looper()
 {
@@ -18,8 +20,6 @@ Looper::~Looper()
   osc = nullptr;
   delete buffer;
   buffer = nullptr;
-  delete writeFile;
-  writeFile = nullptr;
 }
 
 float Looper::applyEffect(float sample)
@@ -30,56 +30,35 @@ float Looper::applyEffect(float sample)
   float interpolatedValue = Interpolation::scale(sine,0,2.1,0,numSamples);
   int intS = (int) interpolatedValue;
   float y = Interpolation::linMap(interpolatedValue,intS,intS+1,buffer[intS],buffer[intS+1]);
-  writeFile->write(std::to_string(y) + "\n");
+  //interpolate between sine value and place in buffer to walk though wav
   return y;
 }
 
 void Looper::setParameter(std::string id, float val)
 {
-
+  if( id == "pickSample")
+  {
+     val;
+    std::cin >> wav;
+    fillBuffer();
+  }
 }
 
 void Looper::fillBuffer()
 {
-  audioFile.load("/Users/Julia/Documents/Atom/HKU/CSD2c/3.les/audio/eigenEffect/samples/OH.wav");
+  audioFile.load(wav);
   audioFile.printSummary();
   numSamples = audioFile.getNumSamplesPerChannel();
   numSamples -= 1;
-  std::cout << "Looper::fillBuffer - numSamples = " << numSamples << std::fixed << std::endl;
+  //else the buffer exeeds how many samples the wav has
   delete buffer;
   buffer = nullptr;
   buffer = new float[numSamples];
   int channel = 0;
-  //else the buffer exeeds how many samples the wav has
   for(int i = 0; i < numSamples; i++)
   {
     currentSample = audioFile.samples[channel][i];
-    // std::cout << "Looper::fillBuffer - currentSample = " << currentSample << std::endl;
-
     buffer[i] = currentSample;
+    //fill the buffer with audio file
   }
-  // applyEffect(0);
-}
-
-float Looper::scale(float y, float x1From, float x2From, float x1To, float x2To)
-{
-  float delta = x2From - x1From;
-  float yScaled = y/delta;
-  std::cout << yScaled << std::endl;
-  float newBufSize = x2To - x1To;
-  float scale = (yScaled * newBufSize) + x1To;
-  return scale;
-}
-
-float Looper::interpolation(float x, float x1, float x2, float y1, float y2)
-{
-  float y = y1 + (y2-y1) * ((x-x1) / (x2-x1));
-  // interpolation(scaledSample,i,i+1,buffer[i],buffer[i+1])
-  // std::cout << "Waveshaper::interpolation - x = " << x << std::endl;
-  // std::cout << "Waveshaper::interpolation - x1 = " <<x1 << std::endl;
-  // std::cout << "Waveshaper::interpolation - x2 = " << x2 << std::endl;
-  // std::cout << "Waveshaper::interpolation - y1 = " << y1 << std::endl;
-  // std::cout << "Waveshaper::interpolation - y2 = " << y2 << std::endl;
-  // std::cout << "Waveshaper::interpolation - y = " << y << std::endl;
-  return y;
 }
--- CSD2c/eindopdracht/looper.h
+++ CSD2c/eindopdracht/looper.h
@@ -4,7 +4,6 @@
 #include "AudioFile.h"
 #include "oscillator.h"
 #include "interpolation.h"
-#include "writeToFile.h"
 
 class Looper : public Effect
 {
@@ -14,17 +13,15 @@ public:
 
   float applyEffect(float sample) override;
   void setParameter(std::string id, float val) override;
-  float scale(float y, float x1From, float x2From, float x1To, float x2To);
-  float interpolation(float x, float x1, float x2, float y1, float y2);
 
 private:
   float* buffer = nullptr;
   Oscillator* osc = nullptr;
-  WriteToFile* writeFile = nullptr;
 
   float numSamples;
   float currentSample;
+  float modFreq;
+  std::string wav;
 
   void fillBuffer();
-
 };
